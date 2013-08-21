356dd686b1c31bd2904908af14963f9c3e4d48e1
Keith Keller <kkeller@lbl.gov>
2013-08-21 18:10:51 -0500

    Support for optional third argument, directing a deprecated script name to its replacement name

--- tools/wrap_perl.sh
+++ tools/wrap_perl.sh
@@ -1,18 +1,15 @@
 #
 # Wrap a perl script for execution in the development runtime environment.
-# Ultimately should be able to emit warnings about deprecated script
-# names to stderr
 #
 
-if [ $# -ne 2 ] ; then
-    echo "Usage: $0 source dest" 1>&2 
+if [ $# -ne 3 ] && [ $# -ne 2 ] ; then
+    echo "Usage: $0 source dest [newname]" 1>&2 
     exit 1
 fi
 
 src=$1
 dst=$2
-
-
+newname=$3
 
 cat > $dst <<EOF
 #!/bin/sh
@@ -20,7 +17,22 @@ export KB_TOP=$KB_TOP
 export KB_RUNTIME=$KB_RUNTIME
 export PATH=$KB_RUNTIME/bin:$KB_TOP/bin:\$PATH
 export PERL5LIB=$KB_PERL_PATH
+
+EOF
+
+if [[ -n $newname ]]
+then
+	base=$(basename $dst)
+	cat >> $dst <<EOF
+echo "Command $base has been deprecated. Please use command $newname instead." >&2
+EOF
+
+fi
+
+cat >> $dst <<EOF
+
 $KB_RUNTIME/bin/perl $src "\$@"
+
 EOF
 
 chmod +x $dst
