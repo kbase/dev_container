e87e8e8d0aa496fc180fe8ce634e2331069c5eae
brettin <brettin@mcs.anl.gov>
2013-11-07 02:09:45 +0000

    added SCRIPT_TYPE, a script that determines if a script is a perl, python or shell script by looking for the . The desired approach should be based on the script suffix (ie .pl). This can be used when there is no suffix.

---
+++ tools/script_type.pl
@@ -0,0 +1,22 @@
+
+
+# check the incoming arg if it is a valid file
+die unless -e $ARGV[0];
+
+# parse out the #! from the file
+open F, $ARGV[0] or die;
+my $sebang;
+while(<F>) {
+
+	$sebang = $_ if /^\#\!\s*/;
+	last if defined $sebang;
+
+}
+close F;
+
+my $type = 'unknown';
+$type = 'sh' if $sebang =~ /sh/;
+$type = 'py' if $sebang =~ /python/;
+$type = 'pl' if $sebang =~ /perl/;
+
+print $type;
