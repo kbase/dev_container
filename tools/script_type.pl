6f394ebc0317d4c1073f43d36103a5c69a657249
brettin <brettin@anl.gov>
2014-03-11 22:33:23 +0000

    merging in changes for script wrap support

--- tools/script_type.pl
+++ tools/script_type.pl
@@ -1,7 +1,7 @@
 
 
 # check the incoming arg if it is a valid file
-die unless -e $ARGV[0];
+die "usage: $0 <script>" unless -e $ARGV[0];
 
 # parse out the #! from the file
 open F, $ARGV[0] or die;
